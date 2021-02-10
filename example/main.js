65d6887e5bdffa73d11009ac8b9f396a46ca6a16
PunPomelo <78335708+PunPomelo@users.noreply.github.com>
2021-02-10 13:49:00 +0700

    initial lambda fn and zip it for terraform

---
+++ example/main.js
@@ -0,0 +1,14 @@
+'use strict';
+const AWS = require('aws-sdk');
+
+exports.handler = function (event, context, callback) {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+    body: '<p>Hello world!</p>',
+  };
+
+  callback(null, response);
+};
